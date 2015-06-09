4670cad2f99a168db2f66dee261105ec5659f66e
Matthew Phillips <matthew@matthewphillips.info>
2015-06-09 11:52:44 -0400

    Works in a webworker now

--- main.js
+++ main.js
@@ -1,13 +1,58 @@
-var sass = require ("sass.js/dist/sass");
+var Sass = require ("sass.js/dist/sass");
 var css = require("$css");
 
 exports.instantiate = css.instantiate;
 exports.buildType = "css";
 
-exports.translate = function(){
-  return Promise.resolve(function(resolve){
-    sass.compile(load.source, function(result){
-      resolve(result);
-    })
+exports.translate = function(load){
+  var base = dir(load.address);
+  var imports = getImports(load.source);
+
+  return getSass(this).then(function(sass){
+    return preload(sass, base, imports);
+  }).then(function(sass){
+    return new Promise(function(resolve){
+      sass.compile(load.source, function(result){
+        resolve(result.text);
+      })
+    });
+  });
+};
+
+function dir(path){
+  var parts = path.split("/");
+  parts.pop();
+  return parts.join("/");
+}
+
+var importExp = /@import.+?"(.+?)"/g;
+function getImports(source){
+  var imports = [];
+  source.replace(importExp, function(whole, imp){
+    imports.push(imp);
+  });
+  return imports;
+}
+
+function preload(sass, base, files){
+  if(!files.length) return Promise.resolve(sass);
+
+  return new Promise(function(resolve){
+    sass.preloadFiles(base, "", files, function(){
+      resolve(sass);
+    });
   });
 }
+
+var getSass = (function(){
+  return function(loader){
+    var np = Promise.resolve(loader.normalize("sass.js/dist/sass.worker", "steal-sass"));
+    return np.then(function(name){
+      return Promise.resolve(loader.locate({ name: name }));
+    }).then(function(url){
+      var sass = new Sass(url);
+      getSass = function() { return sass; };
+      return sass;
+    });
+  };
+})();

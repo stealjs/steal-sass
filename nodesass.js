55189c5e6f49a7072af69fb8b75f36540525d510
Julian Kern <kern.julian@gmx.de>
2016-01-07 14:30:33 +0100

    - set the fully qualified path to the sass.js module - set the correct path to nodesass.js in package.json

--- nodesass.js
+++ nodesass.js
@@ -1,2 +1,4 @@
 var loader = require("@loader");
-module.exports = loader._nodeRequire("sass.js");
+// if we build your app with steal-tools, we are currently not in the steal-sass directory
+// we have to set the fully qualified path to the module
+module.exports = loader._nodeRequire(__dirname + "/node_modules/sass.js/dist/sass.sync.js");
